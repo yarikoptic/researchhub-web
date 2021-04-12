476d38fd0b93b036b6f37fe7ad238e63202f5fa2
Calvin Lee <calvinhlee23@gmail.com>
2021-04-12 11:22:41 -1000

    remove context title from thread bar

--- components/PaperDraftInlineComment/PaperDraftInlineCommentTextWrap.js
+++ components/PaperDraftInlineComment/PaperDraftInlineCommentTextWrap.js
@@ -4,7 +4,7 @@ import Popover from "react-popover";
 import InlineCommentUnduxStore, {
   cleanupStoreAndCloseDisplay,
   findTargetInlineComment,
-  getSavedInlineCommentsGivenBlockKey,
+  getSavedInlineCommentsGivenBlockKeyAndThreadID,
   updateInlineComment,
 } from "./undux/InlineCommentUnduxStore";
 import PaperDraftStore from "../PaperDraft/undux/PaperDraftUnduxStore";
@@ -80,13 +80,7 @@ function PaperDraftInlineCommentTextWrap(
       store: inlineCommentStore,
       updatedInlineComment: newInlineComment,
     });
-    const displayableComments = [
-      newInlineComment,
-      ...getSavedInlineCommentsGivenBlockKey({
-        blockKey,
-        editorState: paperDraftStore.get("editorState"),
-      }),
-    ];
+    const displayableComments = [newInlineComment];
     inlineCommentStore.set("displayableInlineComments")(displayableComments);
     setShowPopover(false);
   };
@@ -106,9 +100,10 @@ function PaperDraftInlineCommentTextWrap(
     event.stopPropagation();
     cleanupStoreAndCloseDisplay({ inlineCommentStore });
     inlineCommentStore.set("displayableInlineComments")(
-      getSavedInlineCommentsGivenBlockKey({
+      getSavedInlineCommentsGivenBlockKeyAndThreadID({
         blockKey,
         editorState: paperDraftStore.get("editorState"),
+        commentThreadID,
       })
     );
     inlineCommentStore.set("animatedTextCommentID")(commentThreadID);
